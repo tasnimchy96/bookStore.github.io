b007eabb542cd2c3eb75c4732c7bcad45d48bd5b
tasnimchy96 <tasnim.achy96@gmail.com>
2023-03-29 16:53:21 +0600

    Fix linters

--- bookStore.js
+++ bookStore.js
@@ -1,48 +1,48 @@
 class BookCollection {
   constructor() {
-    this.books = JSON.parse(localStorage.getItem("books")) || [];
-    this.bookList = document.getElementById("bookList");
-    this.addForm = document.getElementById("addForm");
-    this.addForm.addEventListener("submit", this.addBook.bind(this));
+    this.books = JSON.parse(localStorage.getItem('books')) || [];
+    this.bookList = document.getElementById('bookList');
+    this.addForm = document.getElementById('addForm');
+    this.addForm.addEventListener('submit', this.addBook.bind(this));
     this.displayBooks();
   }
 
   // addBook(event) {
   //   event.preventDefault();
-  //   const title = document.getElementById("title").value;
-  //   const author = document.getElementById("author").value;
+  //   const title = document.getElementById('title').value;
+  //   const author = document.getElementById('author').value;
   //   this.books.push({ title, author });
-  //   localStorage.setItem("books", JSON.stringify(this.books));
+  //   localStorage.setItem('books', JSON.stringify(this.books));
   //   this.displayBooks();
   //   event.target.reset();
   // }
 
   // removeBook(index) {
   //   this.books = this.books.filter((book, i) => i !== index);
-  //   localStorage.setItem("books", JSON.stringify(this.books));
+  //   localStorage.setItem('books', JSON.stringify(this.books));
   //   this.displayBooks();
   // }
 
   displayBooks() {
-    this.bookList.innerHTML = "";
+    this.bookList.innerHTML = '';
     this.books.forEach((book, index) => {
-      const tr = document.createElement("tr");
+      const tr = document.createElement('tr');
       tr.innerHTML = `
         <td>"${book.title}" by ${book.author}</td>
       `;
-      const removeBtn = document.createElement("button");
-      removeBtn.textContent = "Remove";
-      removeBtn.addEventListener("click", () => {
+      const removeBtn = document.createElement('button');
+      removeBtn.textContent = 'Remove';
+      removeBtn.addEventListener('click', () => {
         this.removeBook(index);
       });
 
-      removeBtn.classList.add("removeBtn")
+      removeBtn.classList.add('removeBtn');
       tr.appendChild(removeBtn);
       this.bookList.appendChild(tr);
     });
   }
 }
 
-new BookCollection();
+const bookStore = new BookCollection();
 
-
+bookStore();
