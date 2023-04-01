e6c03334e611e1f91a0c96f26dfc75deb46120ec
tasnimchy96 <tasnim.achy96@gmail.com>
2023-04-01 16:16:49 +0600

    Fix linters

--- navigation.js
+++ navigation.js
@@ -1,36 +1,29 @@
-//button
-const list = document.getElementById("list");
-const addNew = document.getElementById("add-new");
-const contact = document.getElementById("contact");
+// button
+const list = document.getElementById('list');
+const addNew = document.getElementById('add-new');
+const contact = document.getElementById('contact');
 
-//div
-const listInfo = document.getElementById("list-info");
-const addNewInfo = document.getElementById("add-new-info");
-const contactInfo = document.getElementById("contact-info");
+// div
+const listInfo = document.getElementById('list-info');
+const addNewInfo = document.getElementById('add-new-info');
+const contactInfo = document.getElementById('contact-info');
 
 function showList() {
-  console.log("list");
-  listInfo.style.display = "flex";
-  addNewInfo.style.display = "none";
-  contactInfo.style.display = "none";
+  listInfo.style.display = 'flex';
+  addNewInfo.style.display = 'none';
+  contactInfo.style.display = 'none';
 }
 function showAddNew() {
-  console.log("addnew");
-
-  listInfo.style.display = "none";
-  addNewInfo.style.display = "flex";
-  contactInfo.style.display = "none";
+  listInfo.style.display = 'none';
+  addNewInfo.style.display = 'flex';
+  contactInfo.style.display = 'none';
 }
 function showContact() {
-  console.log("conatct");
-
-  listInfo.style.display = "none";
-  addNewInfo.style.display = "none";
-  contactInfo.style.display = "flex";
+  listInfo.style.display = 'none';
+  addNewInfo.style.display = 'none';
+  contactInfo.style.display = 'flex';
 }
 
-list.addEventListener("click", showList);
-
-addNew.addEventListener("click", showAddNew);
-
-contact.addEventListener("click", showContact);
+list.addEventListener('click', showList);
+addNew.addEventListener('click', showAddNew);
+contact.addEventListener('click', showContact);
